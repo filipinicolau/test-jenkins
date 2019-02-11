6faa835dcc8f04066da320460f63b40c4bca6f6d
Filipi <fnicolau@ciandt.com>
2019-02-11 11:21:01 -0200

    Groovy file

--- JenkinsFileJavaDepCI.groovy
+++ JenkinsFileJavaDepCI.groovy
@@ -1,19 +1,14 @@
-pipeline{
-	agent {label "Pipeline Test"}
-
-	stages{
-	    stage('Clone') {
-	        echo 'Cloning....'
-	        git url: 'https://github.com/filipinicolau/test-jenkins.git'
-	    }
-	    stage('Exec orchestrator') {
-	        echo 'Exec....'
-	        bat 'echo funciona'
-	        bat 'C:\\Users\\fnicolau\\AppData\\Local\\Programs\\Python\\Python37-32\\python teste.py'
-	    }
-	    stage('Build') {
-	        echo 'Building....'
-	    }
-
-	}
+node {
+    stage('Clone') {
+        echo 'Cloning....'
+        git url: 'https://github.com/filipinicolau/test-jenkins.git'
+    }
+    stage('Exec orchestrator') {
+        echo 'Exec....'
+        bat 'echo funciona'
+        bat 'C:\\Users\\fnicolau\\AppData\\Local\\Programs\\Python\\Python37-32\\python teste.py'
+    }
+    stage('Deploy') {
+        echo 'Deploying....'
+    }
 }
